63fdbaaa6df346717affbec71e0fa3a795c05a26
Alexandr Shapovalov <a.a.shapovalov@bk.ru>
2023-03-30 21:21:13 +0300

    UniqueTree

--- BinaryThree/BinaryThree.cpp
+++ BinaryThree/BinaryThree.cpp
@@ -5,6 +5,8 @@
 
 class Three
 {
+protected:
+
 	class Element
 	{
 		int Data;
@@ -17,18 +19,19 @@ class Three
 
 		Element(int Data, Element* pLeft = nullptr, Element* pRight = nullptr) :Data(Data), pLeft(pLeft), pRight(pRight)
 		{
-			std::cout << "EConstructor:\t" << this << std::endl;
+				/*std::cout << "EConstructor:\t" << this << std::endl;*/
 		}
 
 		~Element()
 		{
 			{
-				std::cout << "EDestructor:\t" << this << std::endl;
+				/*std::cout << "EDestructor:\t" << this << std::endl;*/
 			}
 		}
 		
 
 		friend class Three;
+		friend class UniqueTree;
 
 	}*Root;
 
@@ -49,7 +52,7 @@ public:
 
 	void insert(int Data,Element *Root)
 	{
-
+		if (this->Root == nullptr)this->Root = new Element(Data);
 		if (Root == nullptr) return;
 
 		if (Data < Root->Data)
@@ -64,10 +67,81 @@ public:
 		}
 	}
 
+	int minValue(Element* Root)const
+	{
+		
+		/*if (Root == nullptr) return 0;
+		if (Root->pLeft == nullptr)return Root->Data;
+		return minValue(Root->pLeft);*/
+		
+		if (Root == nullptr) return 0;
+		/* if (Root->pLeft == nullptr)return Root->Data;
+		 return minValue(Root->pLeft);*/
 
+		return Root->pLeft == nullptr ? Root->Data : minValue(Root->pLeft);
+	}
+
+	int maxValue(Element* Root)const
+	{
+		if (Root == nullptr) return 0;
+
+		return Root->pRight  ? maxValue(Root->pRight):Root->Data;
+	}
+
+
+	void print(Element *Root)const
+	{
+		if (Root == nullptr)return;
+		print(Root->pLeft);
+		std::cout << Root->Data << "\t";
+		print(Root->pRight);
+
+
+	}
+
+	int Count(Element* Root)
+	{
+		if (Root == nullptr)return 0;
+		else  return Count(Root->pLeft) + Count(Root->pRight) + 1;
+	}
+
+	int Sum(Element* Root)const
+	{
+		if (Root == nullptr)return 0;
+		else return  Sum(Root->pLeft) + Sum(Root->pRight) + Root->Data;
+	}
+
+	double Avg()
+	{
+
+		return (double)Sum(Root) / Count(Root);
+	}
 
 };
 
+
+class UniqueTree :public Three
+{
+
+public:
+
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)this->Root = new Element(Data);
+		if (Root == nullptr) return;
+
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else if(Data>Root->Data)
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+};
 int main()
 {
 	int n;
@@ -79,5 +153,22 @@ int main()
 	{
 		three.insert(rand() % 100, three.getRoot());
 	}
+	three.print(three.getRoot());
+
+	UniqueTree three2;
+	for (int i = 0; i < n; i++)
+	{
+		three2.insert(rand() % 100, three2.getRoot());
+	}
+	//three2.print( three2.getRoot());
+
+	std::cout << std::endl;
+	std::cout << "Min value : " << three.minValue(three.getRoot()) << std::endl;
+	std::cout << "Max value : " << three.maxValue(three.getRoot()) << std::endl;
+	std::cout << "Values :" << three.Count(three.getRoot()) << std::endl;
+	std::cout << std::endl;
+	std::cout << "Values2 :" << three2.Count(three2.getRoot()) << std::endl;
+	std::cout << "Sum :" << three.Sum(three.getRoot()) << std::endl;
+	std::cout << "Avg :" << three.Avg() << std::endl;
 }
 
